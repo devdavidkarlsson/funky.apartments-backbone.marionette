ec89d90fd6819aa74b90e187750597d5320917d0
david <david@inkorgen.se>
2015-07-29 10:59:17 +0200

    removed callback to simplify controller

--- assets/javascript/app.subletting.js
+++ assets/javascript/app.subletting.js
@@ -6,19 +6,18 @@ MyApp.Subletting = function(){
   }
 
   var Appartment = Backbone.Model.extend();
+  //var Appartment = Parse.Object.extend("Appartment");
 
   var Appartments = Backbone.Collection.extend({
+  //var Appartments = Parse.Collection.extend({
     model: Appartment,
-    initializeData: function(){
-      var self = this;
-      var callback = function (data){console.log(data); self.reset(data)};
-      S.Appartments.fetch(callback);
 
-    },
-    fetch: function(callback){
-      var result = [{name: "14D"},{name: "14E"}];
-      callback(result);
-      return result;
+    initializeData: function(){
+     var result = [{name: "14D", url: "http://placehold.it/100x150"},{name: "14E", url: "http://placehold.it/100x150"}];
+      //this.query = new Parse.Query(Appartment);
+
+      //var result = this.fetch();
+      this.reset(result);
     }
 
   });
